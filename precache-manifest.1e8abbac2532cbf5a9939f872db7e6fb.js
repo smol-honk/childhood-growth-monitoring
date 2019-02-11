ca5214e47246f2a855064bfa0f695a1a7129104e
Lisa Zhang <lisazhang@email.arizona.edu>
2019-02-11 14:55:32 -0700

    Updates

---
+++ precache-manifest.1e8abbac2532cbf5a9939f872db7e6fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "acc67bda12cd5c9595ac",
+    "url": "/childhood-growth-monitoring/static/js/runtime~main.acc67bda.js"
+  },
+  {
+    "revision": "988ae93cabf55e98b96a",
+    "url": "/childhood-growth-monitoring/static/js/main.988ae93c.chunk.js"
+  },
+  {
+    "revision": "a7b7ad260459974dcec3",
+    "url": "/childhood-growth-monitoring/static/js/1.a7b7ad26.chunk.js"
+  },
+  {
+    "revision": "988ae93cabf55e98b96a",
+    "url": "/childhood-growth-monitoring/static/css/main.c011f421.chunk.css"
+  },
+  {
+    "revision": "ace06304240d631b03f43a7a13034e04",
+    "url": "/childhood-growth-monitoring/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/childhood-growth-monitoring/precache-manifest.85f959383fe99fc7dc2964664225a188.js"
+  "/childhood-growth-monitoring/precache-manifest.1e8abbac2532cbf5a9939f872db7e6fb.js"
 );
 
 workbox.clientsClaim();
